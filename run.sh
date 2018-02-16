dcad9e6d9aa761171e5cb01f6d65055914a35c86
Moosa Ahmed <moosahmed14@gmail.com>
2018-02-16 17:33:59 -0600

    cleaned run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#
-
-#for file in "/drives/c/Users/moosa/PycharmProjects/insight_data/campaign_loyalty_trends/loyalty_trends/insight_testsuite/tests"/*;do
-
-#cd ${file}/../../..
 
 python3 ./src/campaign_loyalty_trends.py ./input/itcont.txt ./input/percentile.txt ./output/repeat_donors.txt 2> /dev/null
